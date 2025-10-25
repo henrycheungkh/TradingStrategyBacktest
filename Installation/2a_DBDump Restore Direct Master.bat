f1066dd2d3b59e33a5fb4e1006db9e8e21640017
henrycheungkh <henry.cheungkh@gmail.com>
2025-10-25 04:13:54 +0100

    modified:   Installation/2a_DBDump Restore Direct Master.bat 	modified:   Installation/2b_DBDump Restore Direct 1min ib.bat 	modified:   Installation/2c_DBDump Restore Direct 30mins ib.bat

--- Installation/2a_DBDump Restore Direct Master.bat
+++ Installation/2a_DBDump Restore Direct Master.bat
@@ -1,23 +1,23 @@
-set DateString=20250927
+set DateString=20251024
 
 SET /P AREYOUSURE=It will overwrite data as of %DateString%, are you sure (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 
 set DBName=fdata_master
-if %TradeAnalysis_DBPassword%==None (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_finance_%DBName%_%DateString%.sql) else (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_finance_%DBName%_%DateString%.sql)
+if %TradeAnalysis_DBPassword%==None ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_finance_%DBName%_%DateString%.sql) else ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_finance_%DBName%_%DateString%.sql)
 
 set DBName=fdata_price_dayend
-if %TradeAnalysis_DBPassword%==None (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql) else (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql)
+if %TradeAnalysis_DBPassword%==None ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql) else ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql)
 
 set DBName=fdata_price_1min_fx
-if %TradeAnalysis_DBPassword%==None (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql) else (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql)
+if %TradeAnalysis_DBPassword%==None ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql) else ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql)
 
 set DBName=fdata_price_dayend_ib
-if %TradeAnalysis_DBPassword%==None (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql) else (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql)
+if %TradeAnalysis_DBPassword%==None ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql) else ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql)
 
 set DBName=fdata_price_30min
-if %TradeAnalysis_DBPassword%==None (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql) else (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql)
+if %TradeAnalysis_DBPassword%==None ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql) else ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%.sql)
 
 
 :END
--- Installation/2b_DBDump Restore Direct 1min ib.bat
+++ Installation/2b_DBDump Restore Direct 1min ib.bat
@@ -1,4 +1,4 @@
-set DateString=20250927
+set DateString=20251024
 
 SET /P AREYOUSURE=It will overwrite data as of %DateString%, are you sure (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
@@ -6,7 +6,7 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 
 set DBName=fdata_price_1min_ib
-if %TradeAnalysis_DBPassword%==None (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql) else (%TradeAnalysis_mysql% -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql)
+if %TradeAnalysis_DBPassword%==None ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql) else ("%TradeAnalysis_mysql%" -u %TradeAnalysis_DBUser% -p%TradeAnalysis_DBPassword% finance_%DBName% < %TradeAnalysis_DBDumpPath%DBDump_%DBName%_%DateString%_ib.sql)
 
 :END
 
