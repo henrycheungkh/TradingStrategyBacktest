99b8a4068e6ea19aeb83fe6f615df583d3157e9b
henrycheungkh <henry.cheungkh@gmail.com>
2025-10-25 02:41:41 +0100

    new file:   Installation/0_setup_tradeanalysis_env_AceMagic.ps1 	new file:   Installation/0_setup_tradeanalysis_env_MainPC.ps1

---
+++ Installation/0_setup_tradeanalysis_env_AceMagic.ps1
@@ -0,0 +1,36 @@
+# ==============================================
+# TradeAnalysis Environment Variable Setup Script
+# ==============================================
+Write-Host "Setting up TradeAnalysis environment variables..." -ForegroundColor Cyan
+
+$envVars = @{
+    "TradeAnalysis_7zip"                       = "C:\Program Files\7-Zip\7z"
+    "TradeAnalysis_CCompilerPath"              = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29910\bin\Hostx64\x64"
+    "TradeAnalysis_CUDAThreadCount"            = "1024"
+    "TradeAnalysis_DBDumpPath"                 = "C:\TradeAnalysisProject\DBDump\"
+    "TradeAnalysis_DBDumpZipPath"              = "C:\TradeAnalysisProject\DBDumpZip\"
+    "TradeAnalysis_DBDumpZipSuffix"            = "Henry"
+    "TradeAnalysis_DBHost"                     = "localhost"
+    "TradeAnalysis_DBPassword"                 = "None"
+    "TradeAnalysis_DBPort"                     = "3306"
+    "TradeAnalysis_DBUser"                     = "root"
+    "TradeAnalysis_DownloadFileBuffer"         = "C:\TradeAnalysisProject\TAHistoricalData\"
+    "TradeAnalysis_DownloadFuturesFileBuffer"  = "C:\TradeAnalysisProject\FuturesHistoricalData\"
+    "TradeAnalysis_IB_API_clientId"            = "123"
+    "TradeAnalysis_IB_API_hostname"            = "127.0.0.1"
+    "TradeAnalysis_IB_API_port"                = "7496"
+    "TradeAnalysis_LocalTimezone"              = "Europe/London"
+    "TradeAnalysis_mysql"                      = "C:\xampp\mysql\bin\mysql"
+    "TradeAnalysis_mysqldump"                  = "C:\xampp\mysql\bin\mysqldump"
+    "TradeAnalysis_ProjectPath"                = "C:\PythonProjects\TradeAnalysis\"
+    "TradeAnalysis_PythonPath"                 = "C:\ProgramData\Anaconda3\python.exe"
+    "TradeAnalysis_USGapperScanEndTime"        = "10:00"
+}
+
+foreach ($key in $envVars.Keys) {
+    $value = $envVars[$key]
+    [Environment]::SetEnvironmentVariable($key, $value, "User")
+    Write-Host "Set $key = $value"
+}
+
+Write-Host "`nAll TradeAnalysis environment variables have been set successfully." -ForegroundColor Green
