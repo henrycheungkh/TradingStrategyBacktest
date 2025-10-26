8108a6d4171270d80dcdf039e5b9a3612f355605
henrycheungkh <henry.cheungkh@gmail.com>
2025-10-26 04:38:54 +0000

    new file:   Installation/1_CreateDB_Live_Temp.sql 	new file:   Installation/1_DropDB_Live.sql

---
+++ Installation/1_DropDB_Live.sql
@@ -0,0 +1,20 @@
+DROP DATABASE finance_fdata_master;
+
+DROP DATABASE finance_fdata_fut_hist;
+DROP DATABASE finance_fdata_fut_hist_10secs;
+DROP DATABASE finance_fdata_fut_hist_5secs;
+
+DROP DATABASE finance_fdata_price_30min_ib;
+DROP DATABASE finance_fdata_price_1min_ib;
+DROP DATABASE finance_fdata_price_dayend_ib;
+
+DROP DATABASE finance_fdata_price_1min_fx;
+DROP DATABASE finance_fdata_price_1min;
+DROP DATABASE finance_fdata_price_30min;
+DROP DATABASE finance_fdata_price_dayend;
+
+DROP DATABASE finance_fdata_crypto_binance;
+
+COMMIT;
+
+
