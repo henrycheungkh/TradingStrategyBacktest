d8f7d72afe5a8ab85d5465bd2248220c0142d962
henrycheungkh <henry.cheungkh@gmail.com>
2025-12-14 03:22:29 +0000

    deleted:    Batch/Daily/Archive/DBDump_Backup_Finance.bat 	deleted:    Batch/Daily/Archive/DailyIBDownload - Copy (2).bat 	deleted:    Batch/Daily/Archive/DailyIBDownload - Copy (3).bat 	deleted:    Batch/Daily/Archive/DailyIBDownload - Copy (4).bat 	deleted:    Batch/Daily/Archive/DailyIBDownload - Copy (5).bat 	deleted:    Batch/Daily/Archive/DailyIBDownload - Copy (6).bat 	deleted:    Batch/Daily/Archive/DailyIBDownload - Copy (7).bat 	deleted:    Batch/Daily/Archive/DailyIBDownload - Copy.bat 	deleted:    Batch/Daily/Archive/DailyIBDownloadTest.bat 	deleted:    Batch/Daily/Archive/Download_PriceFromIB_30min Old.bat 	deleted:    Batch/Daily/Archive/Download_PriceFromIB_30min.bat 	deleted:    Batch/Daily/Archive/Download_PriceFromIB_30min_Patching.bat 	deleted:    Batch/Daily/Archive/Download_PriceFromIB_dayend.bat 	deleted:    Batch/Daily/Archive/Download_Tickers_IPO.bat 	deleted:    Batch/Daily/Archive/SynchronizeGappersList.bat 	deleted:    Installation/archive/1_CreateDB - Copy.sql 	deleted:    Installation/archive/1_CreateDB.sql

--- Batch/Daily/Archive/Download_PriceFromIB_30min.bat
+++
@@ -1,3 +0,0 @@
-rem D:\ProgramData\Anaconda3\python.exe D:\PythonProjects\TradeAnalysis\DownloadUSStockPriceFromIB.py 10000000 2021-07-28 2000 0 "TRADES,BID,ASK" "2 D" DirectUpload
-D:\ProgramData\Anaconda3\python.exe D:\PythonProjects\TradeAnalysis\DownloadUSStockPriceFromIB.py 10000000 2021-07-28 2000 0 "TRADES" "2 D" DirectUpload
-pause
--- Batch/Daily/Archive/Download_PriceFromIB_dayend.bat
+++
@@ -1,2 +0,0 @@
-D:\ProgramData\Anaconda3\python.exe D:\PythonProjects\TradeAnalysis\DownloadUSStockPriceDayEndFromIB.py 10000000 2021-07-28 7000 0 "TRADES" "2 D" DirectUpload
-pause
