1ba1d18c630e469e3418b5b8cc45f37f17f837b4
henrycheungkh <henry.cheungkh@gmail.com>
2025-12-19 15:44:21 +0000

    modified:   Batch/Daily/DailyIBDownload Stock 1min.bat 	modified:   Batch/Daily/Keep_IB_Gateway_Alive.bat

--- Batch/Daily/DailyIBDownload Stock 1min.bat
+++ Batch/Daily/DailyIBDownload Stock 1min.bat
@@ -2,7 +2,7 @@ title IB Stock 1min Download
 SET PYTHONPATH=%TradeAnalysis_ProjectPath%
 
 
-"%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadUSStockPriceFromIB.py" "1 min" 2000 0 "TRADES" "2 D" DirectUpload 20251216
+"%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadUSStockPriceFromIB.py" "1 min" 2000 0 "TRADES" "2 D" DirectUpload 20251218
 
 
 pause
--- Batch/Daily/Keep_IB_Gateway_Alive.bat
+++ Batch/Daily/Keep_IB_Gateway_Alive.bat
@@ -3,6 +3,8 @@ SET PYTHONPATH=%TradeAnalysis_ProjectPath%
 
 :repeat
 
+time /t
+
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\IB\Keep_IB_Gateway_Alive.py"
 
 timeout /t 900
