7b7bff0d9a201df249719c12affb022d3edc36d6
henrycheungkh <henry.cheungkh@gmail.com>
2025-12-22 05:46:59 +0000

    modified:   InvestmentAnalytics/IB/Keep_IB_Gateway_Alive.py

--- InvestmentAnalytics/IB/Keep_IB_Gateway_Alive.py
+++ InvestmentAnalytics/IB/Keep_IB_Gateway_Alive.py
@@ -68,35 +68,40 @@ class IBapi(EWrapper, EClient):
 def run_loop():
 	app.run()
 
-app = IBapi()
-app.connect(IB_API_hostname, IB_API_port, IB_API_KeepAlive_clientId)
+try:
 
-#Start the socket in a thread
-api_thread = threading.Thread(target=run_loop, daemon=True)
-api_thread.start()
+	app = IBapi()
+	app.connect(IB_API_hostname, IB_API_port, IB_API_KeepAlive_clientId)
 
-time.sleep(1) #Sleep interval to allow time for connection to server
+	#Start the socket in a thread
+	api_thread = threading.Thread(target=run_loop, daemon=True)
+	api_thread.start()
 
-#Create contract object
-contract = Contract()
-contract.symbol = 'AAPL'
-contract.secType = 'STK'
-contract.exchange = 'SMART'
-contract.currency = 'USD'
+	time.sleep(1) #Sleep interval to allow time for connection to server
+
+	#Create contract object
+	contract = Contract()
+	contract.symbol = 'AAPL'
+	contract.secType = 'STK'
+	contract.exchange = 'SMART'
+	contract.currency = 'USD'
 
 
-queryTime = datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")
-# app.reqHistoricalData(4102, contract, queryTime, "3 D", "30 mins", "TRADES", 0, 1, False, [])
-app.reqHistoricalData(4102, contract, queryTime, "1 D", "1 day", "TRADES", 0, 1, False, [])
+	queryTime = datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")
+	# app.reqHistoricalData(4102, contract, queryTime, "3 D", "30 mins", "TRADES", 0, 1, False, [])
+	app.reqHistoricalData(4102, contract, queryTime, "1 D", "1 day", "TRADES", 0, 1, False, [])
 
-#Request Market Data
-app.reqMktData(1, contract, '', False, False, [])
+	#Request Market Data
+	app.reqMktData(1, contract, '', False, False, [])
 
-# app.reqContractDetails(1100, contract)  # Replace '1' with a unique request ID
+	# app.reqContractDetails(1100, contract)  # Replace '1' with a unique request ID
 
-while (len(app.df) == 0):
-    time.sleep(10) #Sleep interval to allow time for incoming price data
+	while (len(app.df) == 0):
+		time.sleep(10) #Sleep interval to allow time for incoming price data
 
-print('data in dataframe after sleep is')
-print(app.df)
-app.disconnect()
+	print('data in dataframe after sleep is')
+	print(app.df)
+	app.disconnect()
+
+except:
+	pass
