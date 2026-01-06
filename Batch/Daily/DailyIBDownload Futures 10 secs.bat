f5f4a50e7b9c1ab70fc2e337c989a926536a2dfa
henrycheungkh <henry.cheungkh@gmail.com>
2026-01-06 17:36:14 +0000

    new file:   Batch/Daily/DailyIBDownload - Copy (2).bat 	new file:   Batch/Daily/DailyIBDownload - Copy.bat 	modified:   Batch/Daily/DailyIBDownload Futures 10 secs.bat 	modified:   Batch/Daily/DailyIBDownload Futures 5 secs.bat 	modified:   Batch/Daily/DailyIBDownload Stock 1min.bat 	modified:   Batch/Daily/DailyIBDownload.bat

--- Batch/Daily/DailyIBDownload Futures 10 secs.bat
+++ Batch/Daily/DailyIBDownload Futures 10 secs.bat
@@ -1,12 +1,13 @@
 title Daily IB Download Futures 10 secs
 SET PYTHONPATH=%TradeAnalysis_ProjectPath%
 
-set CaptureDate=20251202
+set CaptureDate=20260105
 
-"%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 ETHUSDRR %CaptureDate%
 
+"%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 RTY %CaptureDate%
 
 goto :skip
+"%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 ETHUSDRR %CaptureDate%
 
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 HE %CaptureDate%
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 ZW %CaptureDate%
@@ -15,7 +16,6 @@ goto :skip
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 10Y %CaptureDate%
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 ES %CaptureDate%
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 NQ %CaptureDate%
-"%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 RTY %CaptureDate%
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 YM %CaptureDate%
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 ZN %CaptureDate%
 "%TradeAnalysis_PythonPath%" "%TradeAnalysis_ProjectPath%InvestmentAnalytics\Batch\DownloadFuturesFromIB.py" "10 secs" "3 D" DirectUpload -1 ZT %CaptureDate%
